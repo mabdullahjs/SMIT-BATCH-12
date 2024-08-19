1547c9a01164867004184654624c5db882657863
mabdullahjs <mabdullah2037@outlook.com>
2024-08-19 17:34:37 +0500

    introduction

---
+++ 02-Javascript/01-Introduction/app.js
@@ -0,0 +1,76 @@
+// console.log("hello javascript!");
+
+
+// data type
+
+//string "" ''
+//number 20 
+//boolean true | false
+
+
+// console.log("asdasdasd@#@$@#24234234")
+
+
+// console.log(typeof("true"))
+
+// ()
+
+// variable number sa start nahi hoga end ma daal skta hain.
+// special character $ _ ka illawa koi bhi use nahi hoga
+// camel case use krainga.
+
+
+// var fullname = "abdullah";
+// var age = 21;
+// var isLoggedIn = true;
+// console.log(fullname , age , isLoggedIn);
+
+
+
+
+
+// var username = "Abdullah"; // declare | initialize
+// console.log(username);
+// var username = "usman" // redeclare
+// console.log(username);
+// username = "ammar" // reassign
+// console.log(username);
+
+
+
+
+
+
+
+
+
+// alert("hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
