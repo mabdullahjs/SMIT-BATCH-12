ecdbb6687f62f3c135ff49354337d88bf7d54bdb
mabdullahjs <mabdullah2037@outlook.com>
2024-08-22 22:08:08 +0500

    operation and expression

---
+++ 02-Javascript/02-Operation&Expressions/app.js
@@ -0,0 +1,103 @@
+// console.log(typeof("hello world"));
+// console.log(typeof(20));
+// console.log(typeof(true)); 
+
+// alert("hello world");
+// var username = prompt("enter your name");
+// console.log(username);
+
+
+
+//Arithmatic Operator
+
+// +
+// -
+// *
+// /
+// %
+
+
+// var num1 = 20;
+// var num2 = 3;
+// console.log("2" + "23"); // concatination
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// console.log(num1 % num2);
+
+// console.log("hello" + "next console" )
+
+
+
+
+//Assignment Operator
+// =
+// +=
+// -=
+// /=
+// *=
+
+// var num = 20;
+// num = num + 30;
+// num += 30;
+// console.log(num);
+
+
+
+
+//Comparision Operator
+
+// >
+// <
+// == 
+// === 
+// !=
+// !== 
+// >=
+// <=
+
+
+// console.log(20 > 30);
+// console.log(20 < 30);
+// console.log(20 == "20");
+// console.log("20" === "20");
+// console.log(20 >= 20);
+// console.log(20 <= 20);
+
+
+// console.log(2 != "2");
+// console.log(2 !== "2");
+
+
+
+// user sa 4 subject ka marks lena hain through prompt, total marks and percentage nikaalni ha 
+
+
+
+
+
+
+var engMarks = prompt("enter english marks");
+var urduMarks = prompt("enter urdu marks");
+var mathMarks = prompt("enter maths marks");
+var comMarks = prompt("enter computer marks");
+
+var totalMarks = 400;
+var obtainedMarks = engMarks + urduMarks + mathMarks + comMarks;
+console.log(obtainedMarks);
+
+
+
+
+
+
+
+
+
+
+//logical Operator
+
+//&& == and
+//|| == OR
