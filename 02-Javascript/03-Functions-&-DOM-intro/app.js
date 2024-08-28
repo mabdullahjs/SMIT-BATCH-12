07a8f60c8db33c3269b2f1408878f9a0ceb6405d
mabdullahjs <mabdullah2037@outlook.com>
2024-08-28 21:09:41 +0500

    functions

---
+++ 02-Javascript/03-Functions-&-DOM-intro/app.js
@@ -0,0 +1,70 @@
+
+
+
+// function hello(){
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+//     console.log("hello world!");
+// }
+
+// hello()
+
+
+
+
+// function sum(num1 , num2){ //parameter receive krna
+//     console.log(num1 + num2);
+// }
+
+// sum(20 , 30); // argument send krna.
+// sum(50 , 50);
+// sum(80 ,60);
+
+
+
+
+
+
+
+
+
+
+
+// var hello;
+// console.log(hello);
+
+
+
+
+
+// function greetUser(username){
+//     console.log("hello " + username)
+// }
+
+// greetUser("abdullah");
+// greetUser();
+
+
+
+
+
+
+// aik function banaan ha jisma user sa uska username or uski age leni ha. agar age 18 sa bari hojaye to "welcome to our gym" warna "get lost"
+
+
+
+function admission(username , age){
+    if(age >= 18){
+        console.log("welcome to the gym " + username)
+    }else{
+        console.log("maalik admission nahi hoga");
+        
+    }
+}
+
+admission("abdullah" , 18);
+admission("usman" , 17);
+admission("huzaifa" , 12);
+admission("ammar" , 21);
+
