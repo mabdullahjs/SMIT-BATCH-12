e86497d18178eec7b1dd13a33ca4ff6605fde012
mabdullahjs <--replace-all>
2024-09-09 16:09:14 +0500

    dom manipulation

---
+++ 02-Javascript/04-DOM-Manipulation/app.js
@@ -0,0 +1,142 @@
+// console.log("hello world");
+
+
+// function greetUser(username) {
+//     console.log("hello " + username);
+//     return "hello " + username
+// }
+
+// var returedVal = greetUser("abdullah")
+// console.log(greetUser("usman"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(sum(20, 30))
+
+
+
+
+
+
+
+// var p = document.querySelector(".para")
+// p.innerHTML = "changed through javascript"
+
+
+
+// var h1 = document.querySelector("#head");
+
+// function changeBulb() {
+
+//     if (h1.innerHTML === "ON") {
+//         h1.innerHTML = "OFF"
+//     } else {
+//         h1.innerHTML = "ON"
+//     }
+// }
+
+
+
+
+
+// var username = document.querySelector("#username");
+// var head = document.querySelector("#head");
+
+
+// function login() {
+//     head.innerHTML = username.value
+//     username.value = ""
+// }
+
+
+
+
+
+// var div = document.querySelector("#report-card");
+// var fullname = document.querySelector("#fullname");
+// var english = document.querySelector("#english");
+// var urdu = document.querySelector("#urdu");
+// var maths = document.querySelector("#maths");
+
+// function calculateMarks() {
+
+
+//     if (+english.value > 100 || +urdu.value > 100 || +maths.value > 100) {
+//         alert("bhai marks to dedo sahi")
+//     } else {
+//         var obtainedMarks = +english.value + +urdu.value + +maths.value
+//         var percentage = obtainedMarks / 300 * 100 + "%"
+//         console.log("full name ===>", fullname.value);
+//         console.log("obtained marks ===>", obtainedMarks);
+//         console.log("Percentage ===>", percentage);
+
+
+//     }
+// }
+
+// div.innerHTML = "<h1>Muhammad Usman </h1> "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var ul = document.querySelector("ul");
+
+ul.innerHTML = "<li>Home</li><li>About</li><li>Contact</li>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
