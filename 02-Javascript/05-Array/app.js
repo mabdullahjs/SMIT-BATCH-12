94f5ecf2ccd066b114dd63e9f6db52a76145299e
mabdullahjs <--replace-all>
2024-09-11 17:14:49 +0500

    array and its methods

---
+++ 02-Javascript/05-Array/app.js
@@ -0,0 +1,270 @@
+// console.log("hello world!");
+
+// var username = "abdullah"
+
+
+// var fruit1 = "mango";
+// var fruit2 = "orange";
+// var fruit3 = "banana";
+// var fruit4 = "apple";
+
+
+
+
+
+
+
+
+
+
+// index 0
+// length 1
+
+// var fruits = ["apple", "banana", "orange", "mango", 23, true];
+
+// console.log(fruits[3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var users = ["abdullah", "usman", "ammar", "raheel", "affan"]
+// console.log(users.length)
+
+
+//push
+//pop
+//unshift
+//shift
+//splice
+//slice
+//split
+//join
+//concat
+//sort
+//reverse
+//indexof
+
+
+
+
+
+
+// var fruits = ["mango", "orange", "watermelon", "banana", "apple", "pineapple", "guava"]
+
+// console.log(fruits[3])
+// fruits.push("melon")
+// fruits.pop()
+// fruits.unshift("melon")
+// fruits.shift()
+
+
+// fruits.splice(2, 4)
+// fruits.splice(5, 1, "nashpati", "update")
+// fruits.splice(4, 0, "strawberry", 1, 2, 3)
+// fruits.splice(6, 1)
+// var newArr = fruits.slice(3, 5)
+// var newArr = fruits.slice(3)
+// console.log(newArr);
+
+
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myName = ["Muhammad", "Abdullah", "Khan"];
+// console.log(typeof (myName));
+
+
+// console.log(myName.join(" "));
+
+
+// var arrStr = myName.join(" ");
+// var arr = arrStr.split(" ");
+// // console.log(arrStr);
+// console.log(arr);
+
+
+
+
+
+
+
+
+// var str = "Azam is intelligent boy";
+// console.log(str.split("e"))
+
+
+
+// var str = "abcdefgh"
+// console.log(str.split("").reverse().join(""));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr1 = [1, 2, 3];
+// var arr2 = [4, 5, 6];
+// var newArr = arr1.concat(arr2);
+// console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var arr = [7, 5, 3, 9, 6, 2, 1, 4];
+// // console.log(arr.sort());
+// // console.log(arr.indexOf(0));
+// console.log(arr.includes(32));
+
+
+
+// var arr = new Array("abc", "def")
+// console.log(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var userPreference = prompt("enter first color")
+var colours = ["purple", "orange", "red", "black"]
+
+// colours.unshift(userPreference);
+console.log(colours);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
