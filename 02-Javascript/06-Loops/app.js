e119d5a8a83c6ff9e45cfd879fcc1efa44bc0ded
mabdullahjs <--replace-all>
2024-09-13 18:00:53 +0500

    loops

---
+++ 02-Javascript/06-Loops/app.js
@@ -0,0 +1,222 @@
+// console.log("hello world!");
+
+// console.log("hello world")
+
+// for loop
+// while loop
+// do while loop
+
+// for (var i = 0; i < 10; i++) {
+//     console.log("hello world", i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var num = 21;
+
+// console.log(num + " * " + 1 + " = " + num * 1);
+// console.log(num + " * " + 2 + " = " + num * 2);
+// console.log(num + " * " + 3 + " = " + num * 3);
+// console.log(num + " * " + 4 + " = " + num * 4);
+// console.log(num + " * " + 5 + " = " + num * 5);
+// console.log(num + " * " + 6 + " = " + num * 6);
+// console.log(num + " * " + 7 + " = " + num * 7);
+// console.log(num + " * " + 8 + " = " + num * 8);
+// console.log(num + " * " + 9 + " = " + num * 9);
+// console.log(num + " * " + 10 + " = " + num * 10);
+
+
+// var num = 2
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(num + " * " + i + " = " + num * i)
+// }
+
+
+
+// var head = document.querySelector("#head-one");
+
+// function changeText() {
+//     console.log("function called")
+// }
+
+
+
+
+
+
+
+
+
+
+// var username = "abdullah";
+// var age = 20;
+
+// console.log("your username is " + username + " and your age is " + age)
+// console.log(`hello world ${username}`);
+
+// console.log(`your username is ${username} and your age is ${age}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("before loop")
+
+// for (var i = 0; i < 10; i++) {
+//     console.log("inside loop")
+// }
+
+// console.log("after loop")
+
+
+
+
+
+
+// for (var i = 1; i <= 100; i++) {
+//     console.log("outside nested loop");
+
+//     for (var j = 1; j <= 10; j++) {
+//         // console.log("inside loop");
+//         console.log(`${i} * ${j} = ${i * j}`);
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// infinite loop
+
+// for(var i = 11; i > 10; i++)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (var i = 10; i < 5; i++) {
+//     console.log(i)
+// }
+
+
+// var i = 0
+
+// while (i < 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// do while loop
+
+
+// var i = 15
+// do {
+//     console.log(i)
+//     i++
+// } while (i < 5)
+
+
+
+
+
+
+
+
+// var div = document.querySelector("div");
+
+// var fruits = ["mango", "banana", "apple", "orange", 23]
+
+// for (var i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+//     div.innerHTML += `${fruits[i]} <br/>`
+// }
+
+
+// array ko screen pa render krwana
+// for loop
+// push or splice
+// template literals
+
+
