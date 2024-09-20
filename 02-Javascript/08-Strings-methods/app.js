c0f9f2c7be100a72cdd1a63bd7b34c948fa3c293
mabdullahjs <--replace-all>
2024-09-20 18:01:09 +0500

    strings methods

---
+++ 02-Javascript/08-Strings-methods/app.js
@@ -0,0 +1,105 @@
+// console.log("hello world!");
+
+
+// toLowerCase
+// toUpperCase
+// length
+// charAt
+// indexOf
+// slice
+// replace
+// replaceall
+// split
+
+
+// var username = "AbffsFEfd"
+var jhoot = "Azam is a good cricketer. He is a fittest player after virat kohli in the world. Azam is looking like a wow. He passed all the fitness test. He followed diet strictly."
+
+// console.log(username.toLowerCase());
+// console.log(username.toUpperCase());
+// console.log(username.length);
+// console.log(username[0]);
+// console.log(username.charAt(0));
+// console.log(username.indexOf("f"));
+// console.log(username.lastIndexOf("f"));
+// console.log(username.slice(4));
+// console.log(jhoot.replace("Azam", "Abdullah shafique"));
+// console.log(jhoot.replaceAll("Azam", "Abdullah shafique"));
+// console.log(jhoot.split(" ").reverse().join(" "));
+// console.log(20 + true)
+var arr = [1, 2, 3]
+arr[6] = "pukhta irrada"
+arr.length = 0
+console.log(arr)
+
+
+
+
+// var cities = ['karachi', 'lahore', 'islamabad', 'peshawar', 'quetta', 'multan', 'hyderabad']
+
+
+
+// var city = document.querySelector("#city");
+
+// function checkCity() {
+
+//     if (cities.indexOf(city.value.toLowerCase()) === -1) {
+//         console.log("maalik galat jaga paida hogaye hoo..")
+//     } else {
+//         console.log("ustaad zabardast ha tum hi zindagi jee rha ho.");
+
+//     }
+
+//     // for (var i = 0; i < cities.length; i++) {
+//     //     if (cities[i] === city.value) {
+//     //         console.log("valid")
+//     //         break
+//     //     } else {
+//     //         console.log("unvalid");
+
+//     //     }
+//     // }
+
+//     city.value = ""
+// }
+
+
+
+
+// var username = "abdullah";
+// var age = 20;
+// var isLoggedIn = true;
+
+// // console.log(typeof (age))
+
+// var hobbies = ["cricket", "swimming", "eating"];
+
+// console.log(typeof(hobbies));
+
+
+// function greetUser (username) {
+//     return `hello ${username}`
+// }
+
+
+// var greetings = greetUser("abdullah")
+// console.log(greetings);
+
+
+// var div = document.querySelector(".item");
+
+// for (var i = 0; i < hobbies.length; i++) {
+//     console.log(hobbies[i])
+//     div.innerHTML += `<h1>${hobbies[i]}</h1>`
+// }
+
+
+
+
+
+
+
+
+
+
+
