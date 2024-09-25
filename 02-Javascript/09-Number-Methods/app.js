2102f4d898f70ba2ac924bf3d86bbc1c9d2c1d4a
Shehzad Iqbal <--replace-all>
2024-09-25 17:59:14 +0500

    date and time

--- 02-Javascript/09-Number-Methods/app.js
+++ 02-Javascript/09-Number-Methods/app.js
@@ -5,7 +5,7 @@
 // var div = document.querySelector("div");
 // function createTable(){
 //     div.innerHTML = ""
-//     for (let i = 1; i <= timesNum.value; i++) {  
+//     for (let i = 1; i <= timesNum.value; i++) {
 //         div.innerHTML += `${tableNum.value} * ${i} = ${tableNum.value * i} <br/>`
 //     }
 // }
@@ -17,24 +17,82 @@
 // }
 
 
-var div = document.querySelector("div");
-var head = document.querySelector("#head");
-var body = document.querySelector("body");
+// var div = document.querySelector("div");
+// var head = document.querySelector("#head");
+// var body = document.querySelector("body");
 
 
-var arr = ["orange" , "yellow" , "black" , "white" , "green" , "red" , "blue" , "#38bdf8" , "#f3e5f4" , "#0f172a"]
+// var arr = ["orange" , "yellow" , "black" , "white" , "green" , "red" , "blue" , "#38bdf8" , "#f3e5f4" , "#0f172a"]
 
-for (let i = 0; i < arr.length; i++) {
-    div.innerHTML += `<button onclick="colorBtn(${i})">${arr[i]}</button>`
-}
+// for (let i = 0; i < arr.length; i++) {
+//     div.innerHTML += `<button onclick="colorBtn(${i})">${arr[i]}</button>`
+// }
 
-function colorBtn (index){
-    console.log("btn clicked" , arr[index]);
-    if(arr[index] === "blue"){
-        body.style.backgroundColor = "#38bdf8"
-        
-    }else{
-        body.style.backgroundColor = arr[index]
+// function colorBtn (index){
+//     console.log("btn clicked" , arr[index]);
+//     if(arr[index] === "blue"){
+//         body.style.backgroundColor = "#38bdf8"
 
-    }
-}
+//     }else{
+//         body.style.backgroundColor = arr[index]
+
+//     }
+// }
+
+
+
+//round
+//ceil
+//floor
+//random
+//parseInt
+//parseFloat
+
+// var num = 20.2;
+// // console.log(Math.round(num))
+// // console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+
+
+// var num = Math.random() * 6
+// console.log(Math.ceil(num));
+
+
+
+
+// var num = "20.53545";
+// // var convertStr = parseInt(num)
+// // var convertStr = parseFloat(num)
+// var convertStr = +num
+// console.log(typeof (convertStr), convertStr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02-Javascript/10-Date-&-Time/app.js
@@ -0,0 +1,65 @@
+// console.log("date and time")
+
+// var rightNow = new Date();
+// var days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"]
+// console.log(rightNow.toString());
+// console.log(days[rightNow.getDay()]);
+// console.log(rightNow.getMonth());
+// console.log(rightNow.getDate());
+// console.log(rightNow.getFullYear());
+// console.log(rightNow.getSeconds());
+// console.log(rightNow.getMilliseconds());
+// console.log(rightNow.getTime() / 1000 / 60 / 60 / 24);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function calculateAge(dateOfBirth) {
+//     var myBirthDate = new Date(dateOfBirth).getTime();
+//     var rightNow = new Date().getTime()
+
+//     var remainingMilliseconds = rightNow - myBirthDate
+//     console.log(Math.floor(remainingMilliseconds / 1000 / 60 / 60 / 24 / 30 / 12));
+// }
+
+
+// calculateAge('November 27 , 2003')
+// calculateAge('October 22 , 1920')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
