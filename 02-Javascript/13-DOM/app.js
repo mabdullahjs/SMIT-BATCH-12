deac946df7d3c8178e5b585bb07e7cec0f25754b
mabdullahjs <mabdullah2037@outlook.com>
2024-10-14 17:58:30 +0500

    dom completed

---
+++ 02-Javascript/13-DOM/app.js
@@ -0,0 +1,47 @@
+// // const head = document.querySelector('.head');
+// const head = document.querySelectorAll('.head');
+// // const head = document.getElementById('head');
+// // const head = document.getElementsByTagName('h1')
+// // const head = document.getElementsByClassName('head')
+
+// console.log(head);
+
+// head[1].innerHTML = "changed though js"
+
+
+// console.log(document.childNodes[0].childNodes[1].childNodes[1])
+
+// const h1 = document.childNodes[0].childNodes[1].childNodes[1]
+
+// console.log(h1.nodeType);
+
+
+// h1.innerHTML = "HELLO"
+
+
+// const div1 = document.querySelector("#div1").childNodes[0].childNodes[1].childNodes[0];
+
+// // console.log(div1.childNodes)
+// console.log(div1.nodeValue)
+
+
+
+const ul = document.querySelector("#ul");
+
+// ul.innerHTML = `<li>karachi</li>
+//       <li>Islamabad</li>
+//       <li>Lahore</li>
+//       <li>Shikarpur</li>`
+
+
+// const li1 = document.createElement('LI');
+// const li1Text = document.createTextNode("karachi");
+// li1.appendChild(li1Text)
+
+// ul.appendChild(li1)
+
+
+
+// array of object
+// for loop
+// manipulation / rendering
