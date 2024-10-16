526d032e70779191112c55b623a90cd29b50b4a6
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2024-10-16 18:00:33 +0500

    map and foreach

---
+++ 02-Javascript/14-Array-Loops/app.js
@@ -0,0 +1,144 @@
+// callback function
+// higher order function
+
+// map
+// for each
+// filter
+// reduce
+// arrow function
+
+const fruits = ["apple", "grapes", "banana", "orange"];
+
+// console.log(fruits)
+
+const ol = document.querySelector("#ordered-list");
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+//     ol.innerHTML += `<li>${fruits[i]}</li>`
+// }
+
+// function hello(username){ // parameter receive krna
+//     return `hello ${username}`
+// }
+
+// hello('abdullah') // argument pass krna
+
+// kisi bhi function ka argument ma agar ma aik or function dedo, undar dena wala function ko callback function kehta hain.
+
+// fruits.map(function (item , index, arr){
+//     console.log(item , index , arr)
+//     ol.innerHTML += `<li>${item}</li>`
+// })
+
+// const arr = fruits.forEach(function (item , index, arr){
+//     console.log(item , index , arr)
+//     ol.innerHTML += `<li>${item}</li>`
+
+//     return item
+// })
+
+// console.log(arr)
+// const arr = fruits.map(function (item , index, arr){
+//     // console.log(item , index , arr)
+//     ol.innerHTML += `<li>${item}</li>`
+
+//     return item + " hello"
+// })
+
+// console.log(arr)
+
+// const arr = fruits.map(function (item , index){
+//     console.log(item)
+//     return item + ' +'
+// })
+
+// console.log(arr);
+
+// const users = [
+//   { id: 1, name: "Alice", age: 28 },
+//   { id: 2, name: "Bob", age: 34 },
+//   { id: 3, name: "Charlie", age: 22 },
+//   { id: 4, name: "David", age: 40 },
+//   { id: 5, name: "Eva", age: 26 },
+//   { id: 6, name: "Frank", age: 31 },
+//   { id: 7, name: "Grace", age: 29 },
+//   { id: 8, name: "Hank", age: 37 },
+//   { id: 9, name: "Ivy", age: 24 },
+//   { id: 10, name: "Jack", age: 33 },
+// ];
+
+
+// users.map(function (item){
+//     console.log(item)
+//     ol.innerHTML += `<li>${item.name}</li>`
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// arrow function
+
+
+// function greetUser(username){
+//     console.log(`hello ${username}`);
+// }
+ 
+// const calledFunc = greetUser('usman')
+// console.log(greetUser('abdullah'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function sum(num1 , num2){
+//     console.log(num2);
+    
+// }
+
+
+// sum(20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
