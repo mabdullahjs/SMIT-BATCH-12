334ae3848f32982dc321678448fbc9123c92ec07
mabdullahjs <mabdullah2037@outlook.com>
2024-11-06 17:56:08 +0500

    async

---
+++ 02-Javascript/17-Asynchronous/app.js
@@ -0,0 +1,22 @@
+// console.log("hello world 1");
+// console.log("hello world 2");
+// console.log("hello world 3");
+// setTimeout(() => {
+//   console.log("hello world 4");
+// }, 0);
+// console.log("hello world 5");
+
+// settimeout
+// setinterval
+// promises
+// fetch
+// async await
+// axios
+
+// const data = dabba ma sa data mangwaya
+// console.log(data)
+
+// const data = datamangwalo
+// setTimeout(()=>{
+//     console.log(data)
+// } , 1000)
