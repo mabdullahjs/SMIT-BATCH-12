0d1ae4c31ed7541ece6ddd752a5cdd9be94f7f13
mabdullahjs <mabdullah2037@outlook.com>
2024-11-29 17:48:59 +0500

    es6 started

---
+++ 02-Javascript/18-ES6/app.js
@@ -0,0 +1,103 @@
+// Variable Scoping //c
+// Closure 
+// lexical scoping 
+// Modules
+// Object Methods (keys,values,freez,entries)
+// Exponentiation Operator 
+// Optional chaining 
+// Destructuring 
+// Template literals 
+// Destructuring 
+// Default parameters 
+// Rest parameter 
+// Spread Operator 
+// Arrow functions  //c
+// Enhanced object literals  
+// Iterators & For..of  
+// Map  //c
+// Array methods  //c
+// Higher-order function //c  
+// CallBack //c  
+// Promises  //c
+// Ternary Operator //c
+
+
+
+
+// Ternary Operator 
+
+// const age = 17;
+// if (age > 18) {
+//     console.log("you are able")
+// }else if (age > 15) {
+//     console.log("thora sa krskta ho");
+// }else {
+//     console.log("you are unable");
+// }
+
+// age > 18  ? console.log("you are able") : age > 15 ? console.log("thora sa krskta ho") : console.log("you are unable")
+
+
+// Variable Scoping 
+
+
+// global variable or local variable
+
+// function hello (){
+//     var a = 10;
+// }
+
+// console.log(a);
+
+
+
+// functional Scope | block Scope
+
+
+// if(true) {
+//     var a = 10;
+// }
+
+// console.log(a);
+
+
+
+// Spread Operator 
+
+// const arr = [1, 2, 3]
+// const newArr = [...arr , 5 ,6 ,7 , ...arr]
+// console.log(newArr);
+
+
+// const user = {
+//     email: 'mabdullah@gmail.com',
+//     age: 21
+// }
+
+// const newObj = {
+//     username: 'mabdullah',
+//     ...user 
+// }
+
+// console.log(newObj);
+
+
+
+// Default parameters 
+// Rest parameter 
+
+// function hello (username = "usman"){
+//     console.log(`hello ${username}`);
+    
+// }
+
+// hello("abdullah")
+
+
+
+// function usernames(...names){
+//     console.log(names);
+    
+// }
+
+// usernames('abdullah' , 'usman' , 'ammar' , 'affan')
