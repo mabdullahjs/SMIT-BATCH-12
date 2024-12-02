30c6d860ee3f23e34aa031efa4e0ee79290ea0ba
mabdullahjs <mabdullah2037@outlook.com>
2024-12-02 17:46:28 +0500

    for of iterator

--- 02-Javascript/18-ES6/app.js
+++ 02-Javascript/18-ES6/app.js
@@ -6,14 +6,14 @@
 // Exponentiation Operator 
 // Optional chaining 
 // Destructuring 
-// Template literals 
+// Template literals //c
 // Destructuring 
-// Default parameters 
-// Rest parameter 
-// Spread Operator 
+// Default parameters //c 
+// Rest parameter //c
+// Spread Operator //c
 // Arrow functions  //c
 // Enhanced object literals  
-// Iterators & For..of  
+// Iterators & For..of  //c
 // Map  //c
 // Array methods  //c
 // Higher-order function //c  
@@ -64,9 +64,12 @@
 
 // Spread Operator 
 
-// const arr = [1, 2, 3]
-// const newArr = [...arr , 5 ,6 ,7 , ...arr]
+// const arr = [1, 2, 3];
+// const arr2 = [5, 6, 7];
+
+// const newArr = [...arr , ...arr2]
 // console.log(newArr);
+
 
 
 // const user = {
@@ -88,7 +91,7 @@
 
 // function hello (username = "usman"){
 //     console.log(`hello ${username}`);
-    
+
 // }
 
 // hello("abdullah")
@@ -97,7 +100,53 @@
 
 // function usernames(...names){
 //     console.log(names);
-    
+
 // }
 
 // usernames('abdullah' , 'usman' , 'ammar' , 'affan')
+
+// const bankBalance = 200000
+
+// 1000000 > millionaire
+// 500000 > road to million
+// 1 -> 500000  Inshallah you will be millionaire
+
+
+// const div = document.querySelector('.container');
+// const arr = [1, 2, 3, 4, 5];
+
+// for (const element of arr) {
+//     console.log(element);  
+//     div.innerHTML += element + "<br/>" 
+// }
+
+
+// const obj = {
+//     username: 'mabdullah',
+//     age: 20,
+//     email: 'mabdullah2037@gmail.com',
+//     isLoggedIn: true
+// }
+
+
+// for (const key in obj) {
+//    console.log(`${key}: ${obj[key]}`); 
+// }
+
+
+
+
+// Enhanced object literals  
+
+// const username = "Muhammad Abdullah";
+// const email = "mabdullah2037@gmail.com";
+// const age = 21;
+
+
+// const obj = {
+//     username: username,
+//     email,
+//     age
+// }
+
+// console.log(obj);
