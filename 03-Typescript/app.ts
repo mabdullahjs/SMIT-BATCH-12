297d9cc415b97e2ad355b47976a3050aacee1ef3
mabdullahjs <38328235+skahmerali@users.noreply.github.com>
2024-12-27 17:56:41 +0500

    typescript started

---
+++ 03-Typescript/app.ts
@@ -0,0 +1,28 @@
+// TS Compiler P
+// Type Annotations
+// Type Inference
+// Type Aliases
+// Union Types
+// Interfaces
+// Type Aliases vs Interface
+// Generics
+// Enums
+// Tuples
+// Type Guards
+// type casting
+
+
+
+//-----Type Annotations-----//
+
+
+// let username: string = "abdullah"
+// username = " 20"
+
+// let age: number = 20;
+// age = 21
+
+// let isLoggedIn: boolean = true;
+// isLoggedIn = false
+
+// let string:any = true
