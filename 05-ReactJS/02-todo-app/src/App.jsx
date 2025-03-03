287b8aef13be7777d34f6b61eb4cd7e3b6a536f6
mabdullahjs <mabdullah2037@outlook.com>
2025-03-03 15:21:44 +0500

    todo ap

--- 05-ReactJS/02-todo-app/src/App.jsx
+++ 05-ReactJS/02-todo-app/src/App.jsx
@@ -52,6 +52,71 @@
 
 
 
+import React, { useRef, useState } from 'react'
+
+function App() {
+  const title = useRef()
+  const description = useRef()
+  const [todo, setTodo] = useState([]);
+
+
+  function addTodo(event) {
+    event.preventDefault()
+    console.log(title.current.value);
+    console.log(description.current.value);
+
+    todo.push({
+      title: title.current.value,
+      description: description.current.value,
+      id: Date.now()
+    })
+    setTodo([...todo])
+  }
+
+  const deleteTodo = (index)=>{
+    console.log(index);
+    todo.splice(index , 1);
+    setTodo([...todo])
+    
+  }
+  const editTodo = (index)=>{
+    console.log(index);
+    const updatedVal = prompt('enter updated value')
+    todo[index].title = updatedVal
+    setTodo([...todo])
+    
+  }
+  return (
+    <>
+      <h1>Hello Todo</h1>
+
+      <form onSubmit={addTodo}>
+        <input type="text" placeholder='title' ref={title} />
+        <br /><br />
+        <input type="text" placeholder='description' ref={description} />
+        <br /><br />
+        <button >add Todo</button>
+      </form>
+      <div>
+        {todo.length > 0 ? todo.map((item , index) => {
+          return <div key={item.id} style={{
+            border: '1px solid black',
+            borderRadius: '20px',
+            padding: '20px',
+            margin: '20px'
+          }}>
+            <h3>title: {item.title}</h3>
+            <h3>desc: {item.description}</h3><br />
+            <button onClick={()=>deleteTodo(index)}>delete</button>
+            <button onClick={()=>editTodo(index)}>edit</button>
+          </div>
+        }) : <h1>No todo found...</h1>}
+      </div>
+    </>
+  )
+}
+
+export default App
 
 
 
