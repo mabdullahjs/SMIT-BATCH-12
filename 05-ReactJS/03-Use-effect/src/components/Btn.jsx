8673167c970262814c05eedc7bbeafeea1531728
mabdullahjs <mabdullah2037@outlook.com>
2025-03-07 15:35:56 +0500

    reusable components

--- 05-ReactJS/03-Use-effect/src/components/Btn.jsx
+++ 05-ReactJS/03-Use-effect/src/components/Btn.jsx
@@ -1,18 +1,51 @@
+// import React from 'react'
+
+// function Btn(props) {
+//   return (
+//     <button style={{
+//         padding: '5px 20px',
+//         borderRadius: '5px',
+//         border: 'none',
+//         cursor: 'pointer',
+//         backgroundColor: props.bgColor ? props.bgColor : 'red',
+//         color: props.color ? props.color : 'white',
+//         margin: '5px',
+//         fontSize: 20
+//     }}>{props.title}</button>
+//   )
+// }
+
+// export default Btn
+
+
+
+
+
+
+
+
 import React from 'react'
 
-function Btn(props) {
+function Btn({title , bgcolor, color , width, padding, func}) {
   return (
+    <>
     <button style={{
-        padding: '5px 20px',
-        borderRadius: '5px',
-        border: 'none',
-        cursor: 'pointer',
-        backgroundColor: props.bgColor ? props.bgColor : 'red',
-        color: props.color ? props.color : 'white',
-        margin: '5px',
-        fontSize: 20
-    }}>{props.title}</button>
+      backgroundColor: bgcolor,
+      color,
+      width,
+      padding,
+      fontSize: '30px',
+      margin: '30px'
+
+    }} onClick={func}>{title}</button>
+    </>
   )
 }
 
 export default Btn
+
+
+
+
+
+
